dd3a0978bf015969a0ce8cafd04d4c9dd2c419df
Muhammad Iqbal Adenan <kumpultugas17@gmail.com>
2021-10-11 15:56:00 +0800

    Pertemuan 11 (Latihan Logika)

--- mik4/pertemuan11/index.php
+++ mik4/pertemuan11/index.php
@@ -5,6 +5,15 @@
     <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        button {
+            background-color: aqua;
+            border-style: solid;
+            border-color: white;
+            border-width: 1px;
+            height: 30px;
+        }
+    </style>
 </head>
 
 <body>
